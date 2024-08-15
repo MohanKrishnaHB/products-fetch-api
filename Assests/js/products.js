f92e3d29c1021b4fad640507c0c47104be162837
Mohan Krishna <mohankrishnahb@gmail.com>
2024-08-15 21:40:25 +0530

    updated style

--- Assests/js/products.js
+++ Assests/js/products.js
@@ -22,8 +22,8 @@ function designProductElementByProductjson(product) {
                             <img src="${product.image}" alt="Product 1" class="img-fluid">
                         </div>
                         <div class="product-details-container">
-                            <h3 class="h3 mb-0 product-title">${product.title}</h3>
-                            <p class="small product-description mb-1">${product.description}</p>
+                            <h3 class="h3 mb-0 product-title" title="${product.title}">${product.title}</h3>
+                            <p class="small product-description mb-1" title="${product.description}>${product.description}</p>
                             <p class="h6">Rs. ${product.price}</p>
                         </div>
                     </div>
